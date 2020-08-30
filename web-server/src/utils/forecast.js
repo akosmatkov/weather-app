25ff750d66aeb39e873bad2b5653e9efd95dbbf3
Aleksandr Kosmatkov <aleksandrkosmatkov@gmail.com>
2020-08-30 13:28:21 +0300

    added humidity to forecast

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -10,7 +10,10 @@ const forecast = (latitude, longitude, callback) => {
             console.log(url);
             callback("Unable to find location!", undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is " + body.current.temperature + " degrees. It feels like " + body.current.feelslike + " degrees.");
+            callback(undefined, body.current.weather_descriptions[0] + ". It is " + 
+            body.current.temperature + " degrees. It feels like " + 
+            body.current.feelslike + " degrees. The humidity is " + 
+            body.current.humidity + "%");
         }
     })
 }
